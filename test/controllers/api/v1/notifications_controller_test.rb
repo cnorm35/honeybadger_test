c9f7b66a8be96473b78238e1598f7ce209020e08
Cody Norman <wcodynorman@gmail.com>
2023-03-02 06:34:20 -0700

    Add test payloads to Notifications controller test

--- test/controllers/api/v1/notifications_controller_test.rb
+++ test/controllers/api/v1/notifications_controller_test.rb
@@ -4,6 +4,31 @@ module Api
   module V1
     class NotificationsControllerTest < ActionDispatch::IntegrationTest
       setup do
+        @alert_payload = {
+          "RecordType": "Bounce",
+          "Type": "SpamNotification",
+          "TypeCode": 512,
+          "Name": "Spam notification",
+          "Tag": "",
+          "MessageStream": "outbound",
+          "Description": "The message was delivered, but was either blocked by the user, or classified as spam, bulk mail, or had rejected content.",
+          "Email": "zaphod@example.com",
+          "From": "notifications@honeybadger.io",
+          "BouncedAt": "2023-02-27T21:41:30Z",
+        }
+
+        @no_alert_payload = {
+          "RecordType": "Bounce",
+          "MessageStream": "outbound",
+          "Type": "HardBounce",
+          "TypeCode": 1,
+          "Name": "Hard bounce",
+          "Tag": "Test",
+          "Description": "The server was unable to deliver your message (ex: unknown user, mailbox not found).",
+          "Email": "arthur@example.com",
+          "From": "notifications@honeybadger.io",
+          "BouncedAt": "2019-11-05T16:33:54.9070259Z",
+        }
       end
 
       test "new_returns_200" do

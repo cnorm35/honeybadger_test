c310d6147b2523da169d15f23fe10dbbdf0d7613
Cody Norman <wcodynorman@gmail.com>
2023-03-02 06:17:55 -0700

    Move Notifications module within Api module

---
+++ app/controllers/api/v1/notifications_controller.rb
@@ -0,0 +1,8 @@
+module Api
+  module V1
+    class NotificationsController < BaseController
+      def new
+      end
+    end
+  end
+end
---
+++ test/controllers/api/v1/notifications_controller_test.rb
@@ -0,0 +1,13 @@
+require "test_helper"
+
+module Api
+  module V1
+    class NotificationsControllerTest < ActionDispatch::IntegrationTest
+      setup do
+      end
+
+      test "new" do
+      end
+  end
+end
+
